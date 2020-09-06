665f7f72695c4a74b64a41d23d4e6ae1948b0143
a-rusak <alexander.rusak@gmail.com>
2020-09-06 22:00:45 +0300

    Add unit tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,9 +1,48 @@
-import { mount } from "@vue/test-utils";
+import { mount, createLocalVue } from "@vue/test-utils";
 import Calculator from "@/App";
 
-describe("Calculator", () => {
-  it("rendered", () => {
-    const wrapper = mount(Calculator);
-    expect(wrapper.find("form").exists()).toBe(true);
+let wrapper;
+const localVue = createLocalVue();
+
+// region Data
+const DATA = {
+  "+-*/": [
+    ["2+2", 4],
+    ["4-2", 2],
+    ["123.45*67.89", 8381.0205],
+    ["99999/0.123", 813000]
+  ]
+};
+// endregion
+
+beforeEach(() => {
+  wrapper = mount(Calculator, {
+    localVue
+  });
+});
+
+afterEach(() => {
+  wrapper.destroy();
+});
+
+it("Calculator rendered", () => {
+  expect(wrapper.find("form").exists()).toBe(true);
+});
+
+describe("Calculator can add, subtract, multiply, divide numbers", () => {
+  it.each(DATA["+-*/"])("%s = %i", async (expression, expected) => {
+    await wrapper.setData({ expression });
+    expect(wrapper.vm.result).toBe(expected);
+  });
+});
+
+describe("Calculate edge cases", () => {
+  it("Infinity", async () => {
+    await wrapper.setData({ expression: "1e500" });
+    expect(wrapper.vm.result).toBe(Infinity);
+  });
+  it("Round problem", async () => {
+    await wrapper.setData({ expression: ".1+.2" });
+    expect(wrapper.vm.result).toBe(0.3);
   });
 });
