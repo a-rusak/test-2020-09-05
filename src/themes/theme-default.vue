01c168db700bb6614e2ffc3d6a04c719d1e91683
a-rusak <alexander.rusak@gmail.com>
2020-09-14 10:41:37 +0300

    Add button hover, focus styles

--- src/themes/theme-default.vue
+++ src/themes/theme-default.vue
@@ -19,14 +19,15 @@ export default {
 :root {
   --theme-color-bg: var(--pl-color-black-035);
   --theme-color-border: var(--pl-color-black-000);
-  --theme-color-border-inside: var(--pl-color-white-100);
-  --theme-opacity-border-inside: 0.3;
+  --theme-color-border-inside: var(--pl-color-white-opacity-030);
   --theme-color-text: var(--pl-color-white-100);
   --theme-radius-border: 6px;
   --theme-color-bg-button-regular: var(--pl-color-grey-052);
   --theme-color-text-button-regular: var(--pl-color-white-093);
   --theme-color-bg-button-dark: var(--pl-color-black-041);
   --theme-color-bg-button-accent: var(--pl-color-amber-060);
+  --theme-color-border-button-focus: var(--pl-color-white-opacity-050);
+  --theme-color-border-button-hover: var(--pl-color-white-opacity-030);
   --theme-size-padding: var(--pl-size-base);
   --theme-box-shadow: var(--pl-box-shadow-large);
   --theme-color-bg-header: var(--pl-color-white-093);
